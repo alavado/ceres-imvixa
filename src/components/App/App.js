4904827ccfc96eb08e4bfb94241563f5c536c6ba
Alejandro Lavado <alejandro.lvd@gmail.com>
2019-08-17 18:08:25 -0400

    commit inicial

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+import Contenedor from '../Contenedor';
+
+const App = ()  => {
+  return (
+    <div id="fondo">
+      <Contenedor />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Contenedor/Contenedor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Contenedor.css'
+
+const Contenedor = () => {
+  return (
+    <div id="contenedor">
+      
+    </div>
+  );
+};
+
+export default Contenedor;
