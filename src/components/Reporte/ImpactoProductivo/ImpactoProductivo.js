c89b1303c0de0e371dbfefd8e00f4831bd976fa0
cinai <ceinaipil@gmail.com>
2019-11-09 13:28:56 -0300

    agregar info de ambos objetivos cuando están ambos objetivos en impacto productivo

--- src/components/Reporte/ImpactoProductivo/ImpactoProductivo.js
+++ src/components/Reporte/ImpactoProductivo/ImpactoProductivo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux'
-import { OBJETIVO_PESO } from '../../../helpers/constantes';
+import { OBJETIVO_PESO, OBJETIVO_FECHA } from '../../../helpers/constantes';
 import { redondear } from '../../../helpers/helpers';
 import './ImpactoProductivo.css'
 
@@ -16,11 +16,20 @@ const ImpactoProductivo = ({curvaImvixa, curvaTradicional, biomasaImvixa, biomas
         <div className="columna-impacto-productivo">
           <h1>Estrategia 1</h1>
           <div className="indicadores-impacto-productivo">
-            {objetivos.includes(OBJETIVO_PESO) ?
+            {objetivos.includes(OBJETIVO_PESO) && objetivos.includes(OBJETIVO_FECHA) ?
               <>
                 <h2>{redondear(curvaTradicional.length / 30.0)}</h2>
                 <p>meses ciclo</p>
+                <h2>{redondear(curvaTradicional[curvaTradicional.length - 1] / 1000.0, 2)}</h2>
+                <p>kg a la cosecha</p>
                 <h2>{redondear(biomasaTradicional / 1000, 0)}</h2>
+                <p>toneladas cosechadas</p>
+              </> :
+              objetivos.includes(OBJETIVO_PESO) ?
+              <>
+                <h2>{redondear(curvaImvixa.length / 30.0)}</h2>
+                <p>meses ciclo</p>
+                <h2>{redondear(biomasaImvixa / 1000, 0)}</h2>
                 <p>toneladas cosechadas</p>
               </> :
               <>
@@ -35,7 +44,16 @@ const ImpactoProductivo = ({curvaImvixa, curvaTradicional, biomasaImvixa, biomas
         <div className="columna-impacto-productivo">
           <h1>Estrategia 2</h1>
           <div className="indicadores-impacto-productivo">
-            {objetivos.includes(OBJETIVO_PESO) ?
+            {objetivos.includes(OBJETIVO_PESO) && objetivos.includes(OBJETIVO_FECHA)?
+              <>
+                <h2>{redondear(curvaImvixa.length / 30.0)}</h2>
+                <p>meses ciclo</p>
+                <h2>{redondear(curvaImvixa[curvaImvixa.length - 1] / 1000.0, 2)}</h2>
+                <p>kg a la cosecha</p>
+                <h2>{redondear(biomasaImvixa / 1000, 0)}</h2>
+                <p>toneladas cosechadas</p>
+              </> :
+              objetivos.includes(OBJETIVO_PESO) ?
               <>
                 <h2>{redondear(curvaImvixa.length / 30.0)}</h2>
                 <p>meses ciclo</p>
@@ -54,7 +72,16 @@ const ImpactoProductivo = ({curvaImvixa, curvaTradicional, biomasaImvixa, biomas
         <div className="columna-impacto-productivo">
           <h1>Diferencia</h1>
           <div className="indicadores-impacto-productivo">
-            {objetivos.includes(OBJETIVO_PESO) ?
+            {objetivos.includes(OBJETIVO_PESO) && objetivos.includes(OBJETIVO_FECHA)?
+              <>
+                <h2>{redondear(-curvaTradicional.length / 30.0 + curvaImvixa.length / 30.0)}</h2>
+                <p>meses</p>
+                <h2>{redondear(-curvaTradicional[curvaImvixa.length - 1] / 1000.0 + curvaImvixa[curvaImvixa.length - 1] / 1000.0, 2)}</h2>
+                <p>kg a {redondear(curvaImvixa.length / 30.0)} meses</p>
+                <h2>{redondear(-biomasaTradicional / 1000 + biomasaImvixa / 1000, 0)}</h2>
+                <p>toneladas cosechadas</p>
+              </> :
+              objetivos.includes(OBJETIVO_PESO) ?
               <>
                 <h2>{redondear(-curvaTradicional.length / 30.0 + curvaImvixa.length / 30.0)}</h2>
                 <p>meses</p>
