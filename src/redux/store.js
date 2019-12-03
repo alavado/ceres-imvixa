d28c6bd351db919f94796bd22e33e919c079c9f7
Alejandro Lavado <alejandro.lvd@gmail.com>
2019-12-03 15:44:30 -0300

    nuevo release

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,14 @@ import { saveState, loadState } from '../helpers/localStorage.js'
 import throttle from 'lodash.throttle'
 import centroActions from './centro/actions.js'
 
-const persistedState = loadState()
-// const store = createStore(combineReducers(reducers))
-const store = createStore(
-  combineReducers(reducers),
-  persistedState
-)
+// para release, descomentar linea 9 y comentar de la 10 a la 14
+
+const store = createStore(combineReducers(reducers))
+// const persistedState = loadState()
+// const store = createStore(
+//   combineReducers(reducers),
+//   persistedState
+// )
 
 var ipcRenderer = window.require('electron').ipcRenderer;
 ipcRenderer.on('leer-centros', function (event, centros) {
